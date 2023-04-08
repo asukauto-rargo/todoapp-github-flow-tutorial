afb31e1e371ba28ce48c15828f5ace7685e419b6
宇都明日佳 <utoasuka@utoasukanoMacBook-Pro.local>
2023-04-08 15:44:18 +0900

    色のほか修正を行いました。

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ $("#addTodo").click(function (){
 $(document).on("change", "input[type = checkbox]", function() {
     if($(this).is(":checked")) {
         $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#ccc");
+        $(this).parent().css("transition", "1s");
+        $(this).parent().css("font-size", "1.3rem");
     } else {
         $(this).parent().css("text-decoration", "none");
+        $(this).parent().css("color", "#000");
+        $(this).parent().css("transition", "1s");
+        $(this).parent().css("font-size", "1rem");
     }
 });
